8e73b3a51cb44ec071a01e097286fcd37390596b
chuck1z <adritheman@mail.ugm.ac.id>
2020-12-04 00:17:08 +0700

    Omg
    own fix

--- accordiontest/js/calendar.js
+++ accordiontest/js/calendar.js
@@ -1,72 +1,86 @@
 const date = new Date();
 
 const renderCalendar = () => {
+    date.setDate(1);
     const monthDays = document.querySelector(".days");
 
-const firstDayIndex = date.getDay();
+    const firstDayIndex = date.getDay();
 
-const lastDay= new Date(
-    date.getFullYear(), 
-    date.getMonth() + 1, 0).getDate();
+    const lastDay= new Date(
+        date.getFullYear(), 
+        date.getMonth() + 1, 
+        0
+        ).getDate();
 
-const prevLastDay= new Date(
-    date.getFullYear(), 
-    date.getMonth(), 0
-    ).getDate();
+    const prevLastDay= new Date(
+        date.getFullYear(), 
+        date.getMonth(), 
+        0
+        ).getDate();
 
-const lastDayIndex = new Date(
-    date.getFullYear(), 
-    date.getMonth() + 1, 
-    0
-    ).getDay();
+    const lastDayIndex = new Date(
+        date.getFullYear(), 
+        date.getMonth() + 1, 
+        0
+        ).getDay();
+    
+    const prevLastDayIndex = new Date(
+        date.getFullYear(), 
+        date.getMonth(), 
+        0
+        ).getDay();
 
-const nextDays = 7 - lastDayIndex - 1;
+    var nextDays = null;
 
-const month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",  
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-];
+    if(date.getMonth() != 1 && prevLastDayIndex > 3 && prevLastDayIndex < 6){
+        nextDays = 7 - lastDayIndex - 1;
+    } else { 
+        nextDays = 14 - lastDayIndex - 1; 
+    }
 
-document.querySelector(".date h1").
-innerHTML = month[date.getMonth()];
+    const month = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",  
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-document.querySelector(".date p").
-innerHTML = new Date().toDateString();
+    document.querySelector(".date h1").
+    innerHTML = month[date.getMonth()];
 
-let days = "";
+    document.querySelector(".date p").
+    innerHTML = new Date().toDateString();
 
-for(let x = firstDayIndex; x > 0; x--){
-    days += `<div class="prev-date">${
-        prevLastDay - x + 1
-    }</div>`;
-}
+    let days = "";
 
-for(let i = 1; i<=lastDay; i++){
-    if(i== new Date().getDate() && 
-    date.getMonth() === new Date().getMonth()){
-        days += `<div class="today">${i}</div>`;
-    } else{
-        days += `<div>${i}</div>`;
+    for(let x = firstDayIndex; x > 0; x--){
+        days += `<div class="prev-date">${
+            prevLastDay - x + 1
+        }</div>`;
+    }
+
+    for(let i = 1; i<=lastDay; i++){
+        if(i== new Date().getDate() && 
+        date.getMonth() === new Date().getMonth()){
+            days += `<div class="today">${i}</div>`;
+        } else{
+            days += `<div>${i}</div>`;
+        }
+    }
+
+    for(let j = 1; j <= nextDays; j++){
+        days += `<div class="next-date">${j}</div>`;
+        monthDays.innerHTML = days;
     }
 }
-
-for(let j = 1; j <= nextDays; j++){
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
-}
-}
-
-
 
 document.querySelector('.prev').
 addEventListener('click', () => {
